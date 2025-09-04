0f46b86fbc15401125231884c7da08e83b33877b
Roco0309 <69726673+Roco0309@users.noreply.github.com>
2025-09-04 18:50:54 +0900

    2025.09.04

--- Team Rococo/Assets/Scripts/System/CameraManager.cs
+++ Team Rococo/Assets/Scripts/System/CameraManager.cs
@@ -2,15 +2,27 @@ using UnityEngine;
 
 public class CameraManager : MonoBehaviour
 {
+    GameObject gPlayer;
+    GameObject gCamera;
+    GameObject gCameraArm;
+    GameObject gCameraVector;
+    GameObject gCameraAxis;
+
+    Transform tPlayerTransform;
+    Vector3 vCameraPos;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        gPlayer = SystemData.I.sPlayerInfo.gPlayer;
+        tPlayerTransform = gPlayer.transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        vCameraPos = gCameraArm.transform.position;
+        gCameraArm.transform.position += (tPlayerTransform.position - vCameraPos) * SystemData.I.sPlayerInfo.vMoveSpeed;
     }
 }
