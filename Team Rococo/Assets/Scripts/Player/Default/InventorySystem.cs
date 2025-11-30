2b85a147985aef09194504bac6040b4b1650051b
Roco0309 <69726673+Roco0309@users.noreply.github.com>
2025-11-30 21:48:59 +0900

    AddInventory

---
+++ Team Rococo/Assets/Scripts/Player/Default/InventorySystem.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:fc1ebb0af2ff7446d964df1a8a430cb4137fe6b111a1629980a1dc51b962c41b
+size 557
---
+++ Team Rococo/Assets/Scripts/System/Item.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d014c18cc494681ba8187a82348797efd009f5fbb5690deec008434ffc677e33
+size 150
