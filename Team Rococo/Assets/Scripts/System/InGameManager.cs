edf71aa2abdab63b17924ef22d631f329be52204
Roco0309 <69726673+Roco0309@users.noreply.github.com>
2026-02-20 01:35:08 +0900

    aaa

--- Team Rococo/Assets/Scripts/System/InGameManager.cs
+++ Team Rococo/Assets/Scripts/System/InGameManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7cab2b7ebd2e0ec7450104cba1412e0dab4ecb36beee1cf18448baf9f4378fda
-size 656
+oid sha256:1853200af934abeef05106dd255c12b8b81f065b385e5818d6c1a036f05c10f6
+size 1266
---
+++ Team Rococo/Assets/Scripts/System/Setting.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:cd186bbe58a7174474f06350b39216d3ae42f55d53b01cfe146686aec040bef0
+size 1211
