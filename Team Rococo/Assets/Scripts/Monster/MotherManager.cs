cd91b2b06c963a2d641f6c5bbdaba561daaae69a
Roco0309 <69726673+Roco0309@users.noreply.github.com>
2026-03-10 14:58:42 +0900

    Revert "111"
    This reverts commit 293e8c12760990a97a2e1ff0ff0d1c1ec7113bf1.

--- Team Rococo/Assets/Scripts/Monster/MotherManager.cs
+++ Team Rococo/Assets/Scripts/Monster/MotherManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d2723920d1638cce3ae0e092b79e5a1d2658d83a58db4f67956bb1cb17a0b972
-size 272
+oid sha256:d06b5ab22a4db74cab507c935d2489c0275b1832f2228ba51b370aae77ce25aa
+size 2890
