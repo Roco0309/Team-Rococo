c8e9dc8891f29b1d88b116132e231cfe2438f3d7
Roco0309 <69726673+Roco0309@users.noreply.github.com>
2026-04-04 20:14:35 +0900

    111

--- Team Rococo/Assets/Scripts/Monster/TeddyBear.cs
+++ Team Rococo/Assets/Scripts/Monster/TeddyBear.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:55d13cdb1a8490ee1317d4d86cf966e282db1b1cbb394431922001142c9f3488
-size 3423
+oid sha256:35f3eb9415ffc40cae19e19eb09a21b38574bc0756ed3c17559ba8d91934dcd5
+size 3962
