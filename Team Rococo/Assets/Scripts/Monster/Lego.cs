870f38b8d7b5afed16145fd3ec9933c78e85684f
Roco0309 <69726673+Roco0309@users.noreply.github.com>
2026-04-17 05:20:51 +0900

    111

--- Team Rococo/Assets/Scripts/Monster/Lego.cs
+++ Team Rococo/Assets/Scripts/Monster/Lego.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3b65eb1c7c04419b6ce1bdd5975fad600f3cf34194f9cb2a91c8091137e5668d
-size 695
+oid sha256:9dfb0c5fc7dcc4f8660ea9f7ab0cae1d9abb4d744ab8f0fb27be91d37e8f1e43
+size 1559
