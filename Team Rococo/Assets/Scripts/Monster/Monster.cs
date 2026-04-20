059fb29a968867ac55803497030103ce0330d056
Roco0309 <69726673+Roco0309@users.noreply.github.com>
2026-04-21 02:31:55 +0900

    Revert "1111"
    This reverts commit 182a2c541dcf18755ff093b879e09ba0e1ce9808.

--- Team Rococo/Assets/Scripts/Monster/Monster.cs
+++ Team Rococo/Assets/Scripts/Monster/Monster.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:09440e6e35360365a9d3f5fec5fbb3950a5c71acfca0f493d6f674d29d32ec69
-size 6709
+oid sha256:7813ffb740611c1eec12cc4432a7bc0df8c690710ea3b0d6dd8e0ebb470b172b
+size 6737
