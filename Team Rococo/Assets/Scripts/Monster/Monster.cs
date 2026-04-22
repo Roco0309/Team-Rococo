3304bc08f289a1356da7030c2cd0dcff6b720980
Roco0309 <69726673+Roco0309@users.noreply.github.com>
2026-04-22 20:14:28 +0900

    11

--- Team Rococo/Assets/Scripts/Monster/Monster.cs
+++ Team Rococo/Assets/Scripts/Monster/Monster.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fd735b08c7d2d1aab0852ea1684063c3da5e9435c8602b279a55607ee0bc2e15
-size 6843
+oid sha256:744b59a5fc4f04dce14c361dbd9f3e323d85bd9efa8a026df56a6bc362408e73
+size 7008
