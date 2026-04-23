54b069556a8f3cb112621bedacb310747a8a5d14
YounginH <youngin8443@gmail.com>
2026-04-23 17:23:26 +0900

    bear

--- Team Rococo/Assets/Scripts/Monster/TeddyBear.cs
+++ Team Rococo/Assets/Scripts/Monster/TeddyBear.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b91c0938d7dec7acff14d5960231dda2e515eab3f9bd24f82f816c4c241e5c0a
-size 5603
+oid sha256:d48667a6bcd1deb16077fa1f85770e780b48eaa286b28e3f4f91d5687c9bac49
+size 5172
