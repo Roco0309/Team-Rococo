6e417ececb4c9cb9fd108749839561970cc89858
YounginH <youngin8443@gmail.com>
2026-04-24 11:06:19 +0900

    111

--- Team Rococo/Assets/Scripts/System/InGame/InGameManager.cs
+++ Team Rococo/Assets/Scripts/System/InGame/InGameManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:770228993c25fedc368bb599ad87d7dce16dac9b1c531b35c91324b927c57124
-size 12467
+oid sha256:179fdbc1ec65b35569482b59c6f5f4d5e3b2841f54b0459b0308491a44e80a45
+size 12623
--- Team Rococo/Assets/Scripts/System/InGame/InGamePause.cs
+++ Team Rococo/Assets/Scripts/System/InGame/InGamePause.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c25eca5f0e7629b4cc3f2267d9f74dc063f96b70961cd60cddc4682047119243
-size 1985
+oid sha256:eba4ecaf2e60e2b9eedd0a28da08bee2fa40c0945e1a0a1af9215bb1a03605f8
+size 2455
