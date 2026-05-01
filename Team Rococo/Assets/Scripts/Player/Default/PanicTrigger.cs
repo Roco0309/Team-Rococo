55db4f6369a38139365e4243e01c3ed770eed234
ParkHyeonkyu <gnehqkrtk1043@gmail.com>
2026-05-01 13:34:46 +0900

    Edit Panic Sound and Code

--- Team Rococo/Assets/Scripts/Player/Default/PanicTrigger.cs
+++ Team Rococo/Assets/Scripts/Player/Default/PanicTrigger.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c27321b8f34849e3755fa277c23f4d5e5bfcc5fe3573d12966565ca17ffcc23f
-size 374
+oid sha256:56ecc4c520ff3716d566635bba1a2cd06f49c583eaf380562671c462cbe5b52c
+size 668
