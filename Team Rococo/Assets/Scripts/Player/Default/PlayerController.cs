4f453de73adb428e78ab27acf9d922a88e35a9d2
Roco0309 <69726673+Roco0309@users.noreply.github.com>
2026-07-31 15:07:04 +0900

    bug solution

--- Team Rococo/Assets/Scripts/Player/Default/PlayerController.cs
+++ Team Rococo/Assets/Scripts/Player/Default/PlayerController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:614600cbe772131722440dec93737911ccbf780989e20a772e0eb251d5bf8102
-size 8362
+oid sha256:74352b8a14eb08bc26e7f8ad65ec963b4e7e9c4ce66db5061262fbbbf3c36af4
+size 8535
--- Team Rococo/Assets/Scripts/System/InGame/InGameManager.cs
+++ Team Rococo/Assets/Scripts/System/InGame/InGameManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:20b3346b90b325f24f42cc3063551e05100c2aaa2677a57077b10722278eda5c
-size 25694
+oid sha256:12fc2842ad1a334bc2c0ae5d83fbdef9fca44ea4c7de67c09fb8e8e811f81e3d
+size 25681
